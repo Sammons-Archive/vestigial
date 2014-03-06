106fbcb86cc38808dd1beb594496a05064283402
Sammons <bsammnz@gmail.com>
2014-03-06 08:26:59 -0600

    VIRTUAL

--- vestigial.cc
+++ vestigial.cc
@@ -17,23 +17,37 @@ public:
 	parent(const string name) {
 		m_name = name;
 	};
-	string get_name() const {
-		getCounter++;
-		return m_name;
-	}
-private:
+	virtual string get_name() const = 0 ;
+protected:
 	string m_name;
+private:
+
 	mutable int getCounter = 0; 
 };
+
 
 class childA : parent {
 public:
 	~childA() {
 		cout << this->get_name() << "child" << endl;
 	}
+
+	childA(const string name) : parent(name) {}
 private:
 	friend class factory;
-	childA(const string name) : parent(name) {}
+};
+
+class childC : childA {
+public:
+	~childC() {
+		cout << this->get_name() << "child" << endl;
+	}
+	string get_name() const {
+		return "no access";
+	}
+private:
+	friend class factory;
+	childC(const string name) : childA(name) {}
 };
 
 class childB : parent {
@@ -41,7 +55,9 @@ public:
 	~childB() {
 		cout << this->get_name();
 	}
-
+	string get_name() const {
+		return this->get_name();
+	}
 private:
 	friend class factory;
 	childB(const string name) : parent(name) {}
@@ -49,8 +65,11 @@ private:
 
 class factory {
 public:
-	childA* createChildA(const string name) {
-		return new childA(name);
+	// parent* createParent(const string name) {
+	// 	return new parent(name);
+	// }
+	childC* createChildC(const string name) {
+		return new childC(name);
 	}
 	childB* createChildB(const string name) {
 		return new childB(name);
@@ -61,7 +80,8 @@ int main(int argc, char const *argv[])
 {
 	cout << "suppose I work\n";
 	factory f;
-	unique_ptr<childA> p (f.createChildA("ben"));
+	//unique_ptr<parent> par(f.createParent("james"));
+	// unique_ptr<childA> p (f.createChildA("ben"));
 	/* code */
 	return 0;
 }
