5842e08f17425dd04a4efd239ae900717470ef6a
Sammons <bsammnz@gmail.com>
2014-03-05 20:43:32 -0600

    cannot derive from class with private constructor

--- vestigial.cc
+++ vestigial.cc
@@ -1,26 +1,47 @@
 #include <iostream>
 #include <memory>
+#include <string>
+#include <vector>
 using namespace std;
 
 class Sample {
 public:
-
-private:
-	friend class SampleOwner;
+	~Sample(){
+		cout << name << " died " << endl;
+	};
+	void what() {
+		cout << name << " hat" << endl;
+	}
 	Sample() {};
+private:
+	Sample(const string n) {
+		name = n;
+	};
+	friend class SampleOwner;
+	string name;
 };
+
 
 class SampleOwner {
 public:
-	static Sample* createSample() {
-		return new Sample();
+	static Sample* createSample(const string n) {
+		return new Sample(n);
 	}
+};
+
+class ExtraSample : Sample {
+public:
+private:
+	ExtraSample() {};
 };
 
 int main(int argc, char const *argv[])
 {
 	cout << "suppose I work\n";
-	unique_ptr<Sample> s(SampleOwner::createSample());
+	unique_ptr<Sample> s(SampleOwner::createSample("a"));
+	unique_ptr<Sample> s2(SampleOwner::createSample("b"));
+	s.reset(SampleOwner::createSample("c"));
+	s.swap(s2);
 	/* code */
 	return 0;
 }
