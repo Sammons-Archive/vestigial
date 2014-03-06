a5d7127a15ed17cb1b5ff87b828f6d464c53cc9a
Sammons <bsammnz@gmail.com>
2014-03-05 18:59:22 -0600

    reducing leaps to mild bounds

--- vestigial.cc
+++ vestigial.cc
@@ -1,10 +1,8 @@
-#include "vostracious.h"
-
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	Vostracious* vost;// = Vostracious::Vostral();
+
 	/* code */
 	return 0;
 }
--- vestigial.h
+++
@@ -1,1 +0,0 @@
-#include "vostracious.h"
